9305d598f59467a4cb71ee6ebcc772a794dc6151
Valery Menelas <valerymenelas@gmail.com>
2018-04-02 08:39:41 -0400

    ## 2018-04-02 • Updated app.css, dashboard.html, admin.html, and other pages

--- js/date-picker.js
+++ js/date-picker.js
@@ -1,6 +1,18 @@
 (function($) {
     $('#custom-calendar').datepicker({
         inline: true,
-        sideBySide: true
+        sideBySide: true,
+        todayHighlight: true,
+        beforeShowDay: function(d_date){
+            var d_picker = new Date(d_date);
+            var s_class_highlight = '';
+            console.log(d_picker.getMonth());
+            if( d_picker.getMonth() == '2' && (d_picker.getDate() == '14' || d_picker.getDate() == '22' || d_picker.getDate() == '30') ){
+                s_class_highlight = 'cell_highlight';
+                return { classes: s_class_highlight };
+            } else {
+                return;
+            }
+        }
     });
 })(jQuery);
