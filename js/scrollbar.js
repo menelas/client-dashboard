8ed7111e01fb12d95e35f3fa69fd247def655b25
Valery Menelas <valerymenelas@gmail.com>
2018-04-06 11:47:47 -0400

    ## 2018-04-06 • Changed styles on app.css, and other sub pages

--- js/scrollbar.js
+++ js/scrollbar.js
@@ -1,14 +1,22 @@
 (function($) {
-    $(".news-results, .team-results, .webinars-results").mCustomScrollbar({
+    $(".team-results, .webinars-results").mCustomScrollbar({
         setHeight:500,
         theme:"minimal-dark",
         alwaysShowScrollbar: 1
     });
-})(jQuery);;
 
-(function($) {
+    $(".news-results").mCustomScrollbar({
+        setHeight:580,
+        theme:"minimal-dark",
+        alwaysShowScrollbar: 1
+    })
     $("#events-scrollbar, .document-results").mCustomScrollbar({
         setHeight:550,
+        theme:"minimal-dark",
+        alwaysShowScrollbar: 1
+    });
+    $("#webinars-main, .webinars-results-left").mCustomScrollbar({
+        setHeight:720,
         theme:"minimal-dark",
         alwaysShowScrollbar: 1
     });
