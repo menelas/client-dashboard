6e482d1cfeb6e8605fdae4d87ed8cb4bf788bde3
Valery Menelas <valerymenelas@gmail.com>
2018-04-25 14:29:46 -0400

    • Updated webinars.html to change hover effect and removing the scrolling on desktops

--- js/scrollbar.js
+++ js/scrollbar.js
@@ -4,19 +4,23 @@
         theme:"minimal-dark",
         alwaysShowScrollbar: 1
     });
-
+    $("#search-webinars").mCustomScrollbar({
+        setHeight:500,
+        theme:"minimal-dark",
+        alwaysShowScrollbar: 1
+    });
     $(".news-results").mCustomScrollbar({
         setHeight:580,
         theme:"minimal-dark",
         alwaysShowScrollbar: 1
-    })
-    $("#events-scrollbar, .document-results").mCustomScrollbar({
+    });
+    $("#events-scrollbar, .document-results ").mCustomScrollbar({
         setHeight:550,
         theme:"minimal-dark",
         alwaysShowScrollbar: 1
     });
     $("#webinars-main, .webinars-results-left").mCustomScrollbar({
-        setHeight:720,
+        setHeight:625,
         theme:"minimal-dark",
         alwaysShowScrollbar: 1
     });
